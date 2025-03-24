fe7d20cc935e1d5d1c42f7e88bf4907b61cfe92d
busragiran <bgiranzumrut@academic.rrc.ca>
2025-03-24 17:02:15 -0500

    Add namespaced Admin::DashboardController and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :admins
+  # Devise routes for admin login
+  #devise_for :admins
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # Admin namespace
+  namespace :admin do
+    get "dashboard/index"
+    get 'dashboard', to: 'dashboard#index'
+    resources :products
+    resources :categories
+    root to: "dashboard#index" # Optional: Admin root
+  end
 
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Root path for the main site
+  root to: "welcome#index"
 end
