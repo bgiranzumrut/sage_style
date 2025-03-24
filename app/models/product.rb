21a25299f19325f6f84f15052abcaf3ff53cfa71
busragiran <bgiranzumrut@academic.rrc.ca>
2025-03-24 17:26:33 -0500

    Generate scaffold views and controllers for Product and Category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,3 @@
 class Product < ApplicationRecord
   belongs_to :category
-
-  # Optional: add basic validations
-  validates :name, :description, :price, :stock_quantity, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   devise_for :admins
-  # Devise routes for admin login
-  #devise_for :admins
 
-  # Admin namespace
+  # Temporary public access for development/testing
+  resources :products
+  resources :categories
+
   namespace :admin do
     get "dashboard/index"
     get 'dashboard', to: 'dashboard#index'
     resources :products
     resources :categories
-    root to: "dashboard#index" # Optional: Admin root
+    root to: "dashboard#index"
   end
 
-  # Root path for the main site
   root to: "welcome#index"
 end
