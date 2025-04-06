8e2c0ad20f25d87714b9ce622a229934f0e693df
busragiran <bgiranzumrut@academic.rrc.ca>
2025-04-06 10:55:14 -0500

    Add address and province fields to users for checkout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_06_154155) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_06_154834) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -132,7 +132,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_06_154155) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.string "city"
+    t.string "postal_code"
+    t.integer "province_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["province_id"], name: "index_users_on_province_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
@@ -142,4 +147,5 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_06_154155) do
   add_foreign_key "order_items", "products"
   add_foreign_key "orders", "users"
   add_foreign_key "products", "categories"
+  add_foreign_key "users", "provinces"
 end
